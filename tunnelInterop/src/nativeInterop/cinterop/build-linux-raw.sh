4f3b66e2f185d9e64df0aa87bf5abcd224fc8285
Gleb Gutnik <glebgytnik@gmail.com>
2025-10-26 06:04:03 +0300

    Prepare linux build script

---
+++ tunnelInterop/src/nativeInterop/cinterop/build-linux-raw.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -e
+
+buildStatic()
+{
+     echo "build for $1, $2"
+     make CFLAGS="-arch $2" \
+          LFLAGS="-arch $2 -Wl,-Bsymbolic-functions" static
+
+     local OUTPUT_DIR="../bin_$1_$2"
+     mkdir -p $OUTPUT_DIR
+     local OUTPUT_ARCH_FILE="$OUTPUT_DIR/libhev-socks5-tunnel.a"
+
+     libtool -static -o $OUTPUT_ARCH_FILE \
+                   bin/libhev-socks5-tunnel.a \
+                   third-part/lwip/bin/liblwip.a \
+                   third-part/yaml/bin/libyaml.a \
+                   third-part/hev-task-system/bin/libhev-task-system.a
+     make clean
+}
+
+cd hev-socks5-tunnel
+
+buildStatic linux x86_64
+buildStatic linux arm64
